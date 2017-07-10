d27ce222948bd6fc593da27d3361f53286604787
Marcelo Iepsen <miepsen@horizonte.tv.br>
2017-07-09 22:13:52 -0300

    implementando portfolio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/header/index';
+import About from './components/about/index';
+import Work from './components/work/index';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <Header />
+        <div className="sections">
+	        <About />
+	        <Work />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
